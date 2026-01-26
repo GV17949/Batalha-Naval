c422889117e1c26285f3fad43114de203c122e76
Gabriel Victor <gabrielvictorsantos1001@gmail.com>
2026-01-26 18:15:19 -0300

    Build funcional do jogo

--- JogoHaskell/app/Main.hs
+++ JogoHaskell/app/Main.hs
@@ -1,11 +1,3 @@
-import System.Console.Terminfo (clearScreen)
--- Navio - N
--- Água  - ~
--- Hit   - X
--- Miss  - O
-
--- import System.Random.Stateful (uniformRM, globalStdGen)
-
 main :: IO()
 main = do menuPrincipal
 
@@ -35,25 +27,25 @@ iniciarJogo :: IO()
 iniciarJogo = do
     let tabuleiroUsr = criarTabuleiro
     let tabuleiroCpu = criarTabuleiro
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (head naviosDefault)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (naviosDefault !! 1)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (naviosDefault !! 2)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (naviosDefault !! 3)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (naviosDefault !! 4)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     tabuleiroUsr <- putNavio tabuleiroUsr (naviosDefault !! 5)
-    putStrLn (showTabuleiro tabuleiroUsr)
+    putStrLn (showTabuleiro tabuleiroUsr tabuleiroCpu)
 
     -- Configuração automática do tabuleiro da CPU (fixo por simplicidade)
     tabuleiroCpu <- putNavioManual tabuleiroCpu (head naviosDefault) (0,0) H
@@ -61,26 +53,50 @@ iniciarJogo = do
     tabuleiroCpu <- putNavioManual tabuleiroCpu (naviosDefault !! 2) (5,5) H
     tabuleiroCpu <- putNavioManual tabuleiroCpu (naviosDefault !! 3) (7,7) V
 
-    iniciarCombate tabuleiroUsr tabuleiroCpu
+    jogoLoop tabuleiroUsr tabuleiroCpu
 
-  
-    putStrLn (showTabuleiro tabuleiroCpu)
+jogoLoop :: Tabuleiro -> Tabuleiro -> IO()
+jogoLoop tabuleiroUsr tabuleiroCpu = do
+    putStrLn "Sua vez de atirar!"
+    boardCpu <- makeShot tabuleiroCpu
+    putStrLn (showTabuleiro tabuleiroUsr boardCpu)
+    if checkVictory boardCpu
+      then putStrLn "Parabéns! Você venceu!"
+      else do
+        putStrLn "Vez da CPU..."
+        boardUsr <- makeShotCpu tabuleiroUsr
+        putStrLn (showTabuleiro boardUsr boardCpu)
+        if checkVictory boardUsr
+          then putStrLn "A CPU venceu! Tente novamente."
+          else jogoLoop boardUsr boardCpu
 
-iniciarCombate :: Tabuleiro -> Tabuleiro -> IO()
-iniciarCombate boardJog boardCPU = do
-  let tabuleiroUsr = boardJog
-  let tabuleiroCpu = boardCPU
-  putStrLn "Tabuleiro do oponente"
-  putStrLn (encobrirTabuleiro boardCPU)
-  putStrLn "Em qual coordenada deseja atirar?"
-  tabuleiroCpu <- makeShot tabuleiroCpu
-  putStrLn "Ainda em desenvolvimento"
+checkVictory :: Tabuleiro -> Bool
+checkVictory board = all (all (\x -> x /= 1)) board
+
+makeShotCpu :: Tabuleiro -> IO Tabuleiro
+makeShotCpu tabuleiro = do
+    let coord = gerarCoordenada tabuleiro
+    let resultado = checkHit tabuleiro coord
+    case resultado of
+      2 -> do
+        putStrLn ("CPU acertou na coordenada " ++ show coord ++ "!")
+        return (atualizaIndice tabuleiro coord 2)
+      3 -> do
+        putStrLn ("CPU errou na coordenada " ++ show coord ++ ".")
+        return (atualizaIndice tabuleiro coord 3)
+
+-- Apenas prova de conceito: gera a primeira coordenada válida no tabuleiro
+gerarCoordenada :: Tabuleiro -> Coordenada
+gerarCoordenada tabuleiro = head [ (x,y) | x <- [0..boardSize - 1], y <- [0..boardSize - 1], (obterCoord (x,y) tabuleiro == 0 || obterCoord (x,y) tabuleiro == 1) ]
 
 makeShot :: Tabuleiro -> IO Tabuleiro
 makeShot tabuleiro = do
     coord <- getCoordenada
     let resultado = checkHit tabuleiro coord
     case resultado of
+      -1 -> do
+        putStrLn "Posição já atacada. Tente novamente."
+        makeShot tabuleiro
       2 -> do
         putStrLn "Acertou!"
         return (atualizaIndice tabuleiro coord 2)
@@ -89,16 +105,16 @@ makeShot tabuleiro = do
         return (atualizaIndice tabuleiro coord 3)
 
 checkHit :: Tabuleiro -> Coordenada -> Int
-checkHit tabuleiro (x,y) = 
-  if obterCoord (x,y) tabuleiro == 1
-    then 2 -- Hit
-    else 3 -- Miss
+checkHit tabuleiro (x,y)
+  | obterCoord (x,y) tabuleiro == 2 || obterCoord (x,y) tabuleiro == 3  = -1 -- Posição já atacada
+  | obterCoord (x,y) tabuleiro == 1                                     = 2  -- Hit
+  | otherwise                                                           = 3  -- Miss
 
 atualizaIndice :: Tabuleiro -> Coordenada -> Int -> Tabuleiro
 atualizaIndice tabuleiro (x,y) val =
-  take y tabuleiro ++
-  [take x (tabuleiro !! y) ++ [val] ++ drop (x + 1) (tabuleiro !! y)] ++
-  drop (y+1) tabuleiro
+  take x tabuleiro ++
+  [take y (tabuleiro !! x) ++ [val] ++ drop (y + 1) (tabuleiro !! x)] ++
+  drop (x + 1) tabuleiro
 
 putNavio :: Tabuleiro -> Navio -> IO Tabuleiro
 putNavio tabuleiro navio = do
@@ -166,9 +182,6 @@ getOrient = do
       putStrLn "Orientação Inválida. Tente novamente"
       getOrient
 
-
-
---Não parece estar funcionando 
 checkOverlap :: Tabuleiro -> Navio -> Coordenada -> Orientacao -> Bool
 checkOverlap tabuleiro navio (x,y) orient =
   let tam = tamanho navio
@@ -176,7 +189,6 @@ checkOverlap tabuleiro navio (x,y) orient =
                  V -> [(x + i, y) | i <- [0..tam - 1]]
                  H -> [(x, y + i) | i <- [0..tam - 1]]
   in all (\(cx,cy) -> (tabuleiro !! cx !! cy) == 0) coords
-
 
 data Navio = Navio {
     tipo :: String,
@@ -198,7 +210,7 @@ boardSize = 10
 naviosDefault :: [Navio]
 naviosDefault =
     [ Navio "Porta Aviões" 5,
-      Navio "Couraçado" 4,
+      Navio "Encouraçado" 4,
       Navio "Cruzador 1" 3,
       Navio "Cruzador 2" 3,
       Navio "Submarino 1" 2,
@@ -222,11 +234,8 @@ showElementoEncoberto n
   | n == 2 = 'X'
   | n == 3 = 'O'
 
-showTabuleiro :: Tabuleiro -> String
-showTabuleiro tabuleiro = unlines [ [ showElemento (tabuleiro !! x !! y) | x <- [0..boardSize - 1] ] | y <- [0..boardSize - 1] ]
-
-encobrirTabuleiro :: Tabuleiro -> String
-encobrirTabuleiro board = unlines [ [ showElementoEncoberto (board !! x !! y) | x <- [0..boardSize - 1] ] | y <- [0..boardSize - 1] ]
+showTabuleiro :: Tabuleiro -> Tabuleiro -> String
+showTabuleiro boardUsr boardCpu = unlines [ [ showElemento (boardUsr !! y !! x) | x <- [0..boardSize - 1] ] ++ "    " ++ [ showElementoEncoberto (boardCpu !! y !! x) | x <- [0..boardSize - 1] ] | y <- [0..boardSize - 1] ]
 
 obterCoord :: Coordenada -> Tabuleiro -> Int
 obterCoord (x,y) board =
